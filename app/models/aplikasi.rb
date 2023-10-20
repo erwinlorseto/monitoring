af6998a7e895cb72b0e5aee643c83d85c05d4dbb
ryanb1303/laptop <ryan.brilliant.nirwana@gmail.com>
2023-10-20 09:10:33 +0700

    anotate gem dan tambah cdn bootstrap

--- app/models/aplikasi.rb
+++ app/models/aplikasi.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: aplikasis
+#
+#  id                    :integer          not null, primary key
+#  alamat_web            :string
+#  deskripsi             :string
+#  nama                  :string
+#  nama_penanggung_jawab :string
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
 class Aplikasi < ApplicationRecord
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,5 +68,4 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-  config.sass.inline_source_map = true
 end
